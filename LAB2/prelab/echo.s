a0b283110b0af2b1a69a628972a756e52287a791
Jamie Won <jamie.won.2000@gmail.com>
2020-03-05 15:13:16 -0500

    Edits and commetns with tim

--- LAB2/prelab/echo.s
+++ LAB2/prelab/echo.s
@@ -32,49 +32,39 @@ main::
 
 
 PrintString:
-            subi    sp, sp, 8               # adjust stack pointer down to reserve space
-            stw     r3, 4(sp)               # save value of r3 on stack
-            stw     r4, 0(sp)               # save value of r4 on stack
-            movia   r3, r2                  # point to memory address in str_ptr (assumed to be r2)
+            subi    sp, sp, 4               # adjust stack pointer down to reserve space
+            stw     r3, 0(sp)               # save value of r3 on stack
 
+	        mov     r2, r3                  # move r3 to r2   
 ps_loop:
-            ldw     r4, r2                  # read byte at str_ptr address  
+            ldw     r2, 0(r3)               # read byte at str_ptr address  
 
-            ps_IF:  
-                    # do we need to and r4 with some address?
-                    bne r4, r0, ps_ELSE     # if r4 is 0
-            ps_THEN:
-                    br ps_ENDIF             # if equal to 0, end if
-            ps_ELSE:
-                    movi r2, r4             # put byte in r2
-                    call PrintChar          # call printChar
-                    call ps_loop            # loop again
-            ps_ENDIF:
+            beq     r2, r0, end_loop        # if r4 is 0, end loop
 
-            andi    r3, r3, 1               # increment string pointer
+            call    PrintChar               # else call printChar
 
-            ldw     r3, 4(sp)               # restore r3 from stack
-            ldw     r4, 0(sp)               # restore r4 from stack
-            addi    sp, sp, 8               # readjust stack pointer to deallocate space
+	        addi    r3, r3, 4               # increment to read next char
+            br      ps_loop                 # loop again
+
+end_loop:
+            ldw     r3, 0(sp)               # restore r3 from stack
+            addi    sp, sp, 4               # readjust stack pointer to deallocate space
             ret                             # return
 
-
 GetChar:
-            subi    sp, sp, 8               # adjust stack pointer down to reserve space
-            stw     r3, 4(sp)               # save value of r3 on stack
-            stw     r4, 0(sp)               # save value of r4 on stack
+            subi    sp, sp, 4               # adjust stack pointer down to reserve space
+            stw     r3, 0(sp)               # save value of r3 on stack
             movia   r3, JTAG_UART_BASE      # point to first memory location on memory-mapped I/O register
 
     
 gc_loop:
-            ldwio   r4, STATUS_OFFSET(r3)   # read bits from status register
-            andhi   r4, r4, 0x8000          # mask lower bits to isolate upper bits
-            beq     r4, r0, pc_loop         # if upper bits are zero, loop again
-            stw     r2, char(r0)            # store in char on stack should this be smth wJTAG? see commented below
-            #stwio   r2, DATA_OFFSET(r3)     # if upper bits aren't zero, write character to data register
-            lldw    r3, 4(sp)               # restore r3 from stack
-            ldw     r4, 0(sp)               # restore r4 from stack
-            addi    sp, sp, 8               # readjust stack pointer to deallocate space
+            ldwio   r2, STATUS_OFFSET(r3)   # read bits from status register
+            andhi   r2, r2, 0x8000          # mask lower bits to isolate upper bits
+            beq     r2, r0, gc_loop         # if upper bits are zero, loop again
+            ldwio   r2, 0(r3)               # read from r3, put in r2
+            ldw     r3, 0(sp)               # restore r3 from stack
+            andlo   r3, r3, 0x00FF          # and lower bits (mask)
+            addi    sp, sp, 4               # readjust stack pointer to deallocate space
             ret     
 
 
@@ -98,7 +88,7 @@ pc_loop:
 # to ensure word alignment
 # then place byte-sized elements
 # (including character strings)
-char:       .skip 4                 # save four bytes for character
+char:       .skip 4                 # save four bytes for character or is only one byte needed...
 MSG: .asciz  "ELEC 274 Lab 2 Echo Program\n\n"
 
     .end
